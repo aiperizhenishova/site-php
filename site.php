9e28e8f11c63f336e1c9ea98a4130ceb033157bf
Aiperi Zhenishova <zhenishovaaiperi@gmail.com>
2025-11-26 18:20:12 +0600

    first commit

---
+++ site.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $name = "John";
+        $age = 18;
+        echo "There once was a man named $name <br>";
+        echo "He was $age years old <br>";
+        echo "He really liked the name $name <br>";
+        echo "But didn't like being $age <br>";
+    ?>
+</body>
+</html>
