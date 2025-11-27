d5d15b941ceebaf0c417066c27ddfa63ed1bfe5f
Aiperi Zhenishova <zhenishovaaiperi@gmail.com>
2025-11-27 15:36:06 +0600

    data types

--- site.php
+++ site.php
@@ -7,12 +7,30 @@
 </head>
 <body>
     <?php
-        $name = "John";
-        $age = 18;
-        echo "There once was a man named $name <br>";
-        echo "He was $age years old <br>";
-        echo "He really liked the name $name <br>";
-        echo "But didn't like being $age <br>";
+        $phrase = "Hello World! <br>";
+        echo strtoupper($phrase);
+        echo strlen($phrase). "<br>";
+        $phrase[0] = "F";
+        echo $phrase;
+        echo str_replace("F", "L", $phrase);
+        echo substr($phrase, 6, 4). "<br>";
+
+        echo "Aiperi" [2]. "<br>";
+
+        $num = 10;
+        $num++;
+        $num += 25;
+        echo $num. "<br>";
+
+        echo abs(-100). "<br>";
+        echo pow(2, 2). "<br>";
+        echo sqrt(100). "<br>";
+        echo max(101, 100). "<br>";
+        echo min(101, 100). "<br>";
+        echo round(3.57). "<br>";
+        echo ceil(5.17). "<br>";
+        echo floor(5.17). "<br>";
+
     ?>
 </body>
 </html>
