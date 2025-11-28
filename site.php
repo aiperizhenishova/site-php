a9177d3fa19af0769c27b3b9b9eaa0b15a70914f
Aiperi Zhenishova <zhenishovaaiperi@gmail.com>
2025-11-28 17:17:36 +0600

    created a form and basic a calculator

--- site.php
+++ site.php
@@ -6,31 +6,13 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        $phrase = "Hello World! <br>";
-        echo strtoupper($phrase);
-        echo strlen($phrase). "<br>";
-        $phrase[0] = "F";
-        echo $phrase;
-        echo str_replace("F", "L", $phrase);
-        echo substr($phrase, 6, 4). "<br>";
+    <form action="site.php" method="get">
+        <input type="number" name="num1">
+        <br>
+        <input type="number" name="num2">
+        <input type="submit">
+    </form>
 
-        echo "Aiperi" [2]. "<br>";
-
-        $num = 10;
-        $num++;
-        $num += 25;
-        echo $num. "<br>";
-
-        echo abs(-100). "<br>";
-        echo pow(2, 2). "<br>";
-        echo sqrt(100). "<br>";
-        echo max(101, 100). "<br>";
-        echo min(101, 100). "<br>";
-        echo round(3.57). "<br>";
-        echo ceil(5.17). "<br>";
-        echo floor(5.17). "<br>";
-
-    ?>
+    Answer: <?php echo $_GET["num1"] + $_GET["num2"]; ?>
 </body>
 </html>
